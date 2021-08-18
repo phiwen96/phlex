bd418f8a80c839cc10d5edbdc6828f3f141bade3
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-08-18 13:37:56 +0200

    a

--- applications/main.cpp
+++ applications/main.cpp
@@ -3,10 +3,59 @@
 #include <ph_file/file.hpp>
 using std::cout, std::endl;
 
-int main (int argc, char** argv)
+
+enum struct APPLICATION_MODE : int
+{
+    CONSOLE = 1,
+    FILE = 2,
+    UNKNOWN
+};
+
+
+
+auto start_console (auto conf) -> int
 {
     
-    cout << argc << endl << argv[0] << endl;
+    while (true)
+    {
+        cout << ">>";
+        
+    }
+    return 0;
+}
+
+
+
+
+int main (int argc, char** argv)
+{
+    auto mode = (APPLICATION_MODE) argc;
+    
+    switch (mode)
+    {
+        case APPLICATION_MODE::CONSOLE:
+        {
+            start_console (2);
+            break;
+        }
+            
+        case APPLICATION_MODE::FILE:
+        {
+            break;
+        }
+            
+        case APPLICATION_MODE::UNKNOWN:
+        {
+            break;
+        }
+            
+        default:
+        {
+            break;
+        }
+    }
+    
+    
 	return 0;
 }
 
